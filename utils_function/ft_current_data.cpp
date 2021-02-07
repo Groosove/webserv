49e3ce3b5c76d923dca18cc62e686a8ca7bf9d3a
Groosove <arturianec2013@gmail.com>
2021-02-07 18:25:21 +0300

    Write function ft_get_time

--- utils_function/ft_current_data.cpp
+++ utils_function/ft_current_data.cpp
@@ -9,26 +9,6 @@
 #include <sys/time.h>
 #include <ctime>
 
-void ft_get_year(tm &timeinfo, long &time) {
-	int year = 1970;
-	for (; time >= 365; ++year)
-		time -= (year % 4 == 0) ? 366 : 365;
-	timeinfo.tm_year = year;
-	bool leap = timeinfo.tm_year % 4 == 0;
-	int number_month = 1;
-	while (time >= 28 || (time >= 29 && leap)) {
-		if (number_month == 2)
-			time -= (leap) ? 29 : 28;
-		else if (number_month <= 8)
-			time -= (number_month % 2 != 0) ? 31 : 30;
-		else
-			time -= (number_month % 2 != 0) ? 30 : 31;
-		++number_month;
-	}
-	timeinfo.tm_mon = number_month - 1;
-	timeinfo.tm_mday = time + 1;
-}
-
 int ft_get_day_of_week(tm &timeinfo) {
 	int last_number = timeinfo.tm_year % 100;
 	int year_code = (6 + last_number + last_number / 4) % 7;
@@ -54,7 +34,7 @@ int ft_get_day_of_week(tm &timeinfo) {
 	return day_of_week;
 }
 
-void ft_get_sec_minute_hour(tm &timeinfo, long &time) {
+void ft_get_date(tm &timeinfo, long time) {
 	long tmp = time;
 	while (tmp - 3600 >= 0) { tmp -= 3600; }
 	timeinfo.tm_min = tmp / 60;
@@ -62,22 +42,34 @@ void ft_get_sec_minute_hour(tm &timeinfo, long &time) {
 	time = (time - (timeinfo.tm_sec + timeinfo.tm_min * 60)) / 3600; // Время в часах
 	timeinfo.tm_hour = time % 24 + 3;
 	time = (time - (timeinfo.tm_hour - 3)) / 24;
-}
-
-void ft_get_time(tm& timeinfo, long time) {
-	ft_get_sec_minute_hour(timeinfo, time);
-	ft_get_year(timeinfo, time);
+	int year = 1970;
+	for (; time >= 365; ++year)
+		time -= (year % 4 == 0) ? 366 : 365;
+	timeinfo.tm_year = year;
+	bool leap = timeinfo.tm_year % 4 == 0;
+	int number_month = 1;
+	while (time >= 28 || (time >= 29 && leap)) {
+		if (number_month == 2)
+			time -= (leap) ? 29 : 28;
+		else if (number_month <= 8)
+			time -= (number_month % 2 != 0) ? 31 : 30;
+		else
+			time -= (number_month % 2 != 0) ? 30 : 31;
+		++number_month;
+	}
+	timeinfo.tm_mon = number_month - 1;
+	timeinfo.tm_mday = time + 1;
 	timeinfo.tm_wday = ft_get_day_of_week(timeinfo);
 	timeinfo.tm_year -= 1900;
 }
 
-int main() {
+std::string ft_get_time() {
 	struct timeval time;
 	struct tm timeinfo;
 	char buff[100];
 
 	gettimeofday(&time, NULL);
-	ft_get_time(timeinfo, time.tv_sec + (time.tv_usec / 1000000));
-	strftime(buff, 100, "%a, %Y-%m-%d %X", &timeinfo);
-	std::cout << buff;
+	ft_get_date(timeinfo, time.tv_sec + (time.tv_usec / 1000000));
+	strftime(buff, 100, "%a,  %d %b %Y %X GTM", &timeinfo);
+	return std::string(buff);
 }
