f28f87f4750f73f83f8df5e918897e60b07b9d1b
Groosove <arturianec2013@gmail.com>
2021-02-17 18:49:31 +0300

    micro refactor in file parser

--- includes/VirtualServer.hpp
+++ includes/VirtualServer.hpp
@@ -24,12 +24,12 @@ private:
 	std::string							_host;
 	std::string							_port;
 	std::string							_server_name;
-	std::string							_index;
+	std::vector<std::string>			_error_page;
 	std::map<std::string, Location>		_location;
 	std::map<std::string, std::string> 	_parametr;
 	int									_socket;
 	struct sockaddr_in 					_server_addr;
-	std::string					_complete_response;
+	std::string							_complete_response;
 
 	HTTPRequest*						_request;
 	HTTPResponse*						_response;
@@ -46,6 +46,7 @@ public:
 	void							setHost(const std::string& host);
 	void							setPort(const std::string& port);
 	void							setServerName(const std::string& server_name);
+	void							setErrorPage(const std::string& error_page);
 	void							setSocket(int socket);
 
 	std::map<std::string, Location>& getLocation() { return _location; }
@@ -54,5 +55,4 @@ public:
 	inline const std::string& 		getPort() const	{ return _port; }
 	inline const std::string& 		getServerName() const { return _server_name; }
 	inline int 						getSocket() const { return _socket; }
-	std::vector<std::string>			_error_page;
 };
