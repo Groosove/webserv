6f9689bf7b522d5023cb350b95d9f916d14479a2
Freely Kathryne <sokolovaalin1@gmail.com>
2021-03-04 16:38:52 +0300

    cgi constructor new parameters

--- includes/CGI.hpp
+++ includes/CGI.hpp
@@ -10,20 +10,22 @@
 
 #include "HTTPRequest.hpp"
 #include "VirtualServer.hpp"
+#include "Client.hpp"
 
 class CGI {
+public:
+	CGI(Client* client, VirtualServer* virtualServer, char * path);
+	~CGI();
+
+	void execCGI(HTTPResponse* response);
+	char** getEnv() const { return _env; }
+	void setPathCGI(char * path);
+	char* getPathCGI() const { return _argv[0]; }
+
 private:
-	HTTPRequest _request;
+	HTTPRequest* _request;
+	HTTPResponse* _response;
 	char**	_env;
 	char*	_argv[3];
 	int 	_sizeEnv;
-
-public:
-	CGI(HTTPRequest request, VirtualServer virtualServer);
-	~CGI();
-
-	void execCGI(HTTPResponse response);
-	char** getEnv() const { return _env; }
-	void setPathCGI(char * string);
-	char* getPathCGI() const { return _argv[0]; }
 };
--- srcs/CGI.cpp
+++ srcs/CGI.cpp
@@ -9,16 +9,18 @@
 #include "CGI.hpp"
 #include <utils.hpp>
 #include <fcntl.h>
+#include <Client.hpp>
 
 
-CGI::CGI(HTTPRequest request, VirtualServer virtualServer) {
-	this->_request = request;
+CGI::CGI(Client* client, VirtualServer* virtualServer, char * path) {
+	_request = client->getRequest();
+	_response = client->getResponse();
 	_sizeEnv = 18;
 	_env = (char**)calloc(_sizeEnv, sizeof(char*));
 	_env[0] = ft_strdup("AUTH_TYPE=BASIC");
-	_env[1] = ft_strjoin("CONTENT_LENGTH=", std::to_string(request.getRequestSize()).c_str());
-	if (!(request.getHeaders().find("Content-type")->second.empty())) {
-		_env[2] = ft_strjoin("CONTENT_TYPE=", (request.getHeaders().find("Content-type")->second).c_str());
+	_env[1] = ft_strjoin("CONTENT_LENGTH=", std::to_string(_request->getRequestSize()).c_str());
+	if (!(_request->getHeaders().find("Content-type")->second.empty())) {
+		_env[2] = ft_strjoin("CONTENT_TYPE=", (_request->getHeaders().find("Content-type")->second).c_str());
 	} else {
 		_env[2] = ft_strdup("CONTENT_TYPE=");
 	}
@@ -27,17 +29,18 @@ CGI::CGI(HTTPRequest request, VirtualServer virtualServer) {
 	_env[5] = ft_strjoin("PATH_TRANSLATED=", getPathCGI());
 	_env[6] = ft_strdup("QUERY_STRING="); //Строка запроса, если есть, через которую была открыта страница
 	_env[7] = ft_strdup("REMOTE_ADDR="); //IP-адрес, с которого пользователь просматривает текущую страницу
-	_env[8] = ft_strdup("REMOTE_IDENT="); // часть от аутентификации пользователя ??? //
-	_env[9] = ft_strdup("REMOTE_USER="); //Аутентифицированный пользователь //
-	_env[10] = ft_strjoin("REQUEST_METHOD=", request.getMethod());
-	_env[11] = ft_strjoin("REQUEST_URI=", request.getHostUrl());
+	_env[8] = ft_strdup("REMOTE_IDENT=");
+	_env[9] = ft_strdup("REMOTE_USER=");
+	_env[10] = ft_strjoin("REQUEST_METHOD=", _request->getMethod());
+	_env[11] = ft_strjoin("REQUEST_URI=", _request->getHostUrl());
 	_env[12] = ft_strjoin("SCRIPT_NAME=", getPathCGI());
-	_env[13] = ft_strjoin("SERVER_NAME=", virtualServer.getServerName().c_str());
-	_env[14] = ft_strjoin("SERVER_PORT=", std::to_string(virtualServer.getSocket()).c_str());
-	_env[15] = ft_strjoin("SERVER_PROTOCOL=", request.getVersionHTTP());
+	_env[13] = ft_strjoin("SERVER_NAME=", virtualServer->getServerName().c_str());
+	_env[14] = ft_strjoin("SERVER_PORT=", std::to_string(virtualServer->getSocket()).c_str());
+	_env[15] = ft_strjoin("SERVER_PROTOCOL=", _request->getVersionHTTP());
 	_env[16] = ft_strdup("SERVER_SOFTWARE=");//Строка идентификации сервера, указанная в заголовках, когда происходит ответ на запрос
 	_env[17] = nullptr;
-	setPathCGI(ft_strdup("/Users/fkathryn/Desktop/webserv/cgi_scripts/test.py"));
+	setPathCGI(path);
+	execCGI(_response);
 }
 
 CGI::~CGI() {
@@ -50,12 +53,12 @@ CGI::~CGI() {
 	}
 }
 
-void CGI::execCGI(HTTPResponse response) {
+void CGI::execCGI(HTTPResponse* response) {
 	int fd_out;
 	pid_t pid;
 	int file_fd;
 	char buf;
-	char* result_buf = (char*)calloc(10000, sizeof(char));
+	char* result_buf = (char*)calloc(100000, sizeof(char));
 
 	fd_out = dup(1);
 	file_fd = open("file", O_CREAT | O_RDWR | O_TRUNC, 0666);
@@ -67,7 +70,7 @@ void CGI::execCGI(HTTPResponse response) {
 		;//error
 	}
 	else {
-		write(fd_out, _request.getBody(), _request.getRequestSize());
+		write(fd_out, _request->getBody(), _request->getRequestSize());
 		wait(nullptr);
 		lseek(file_fd, 0, 0);
 		int r, size = 0;
@@ -78,9 +81,10 @@ void CGI::execCGI(HTTPResponse response) {
 		result.first = result_buf;
 		result.second = size;
 		//delete old response
-		response.setBody(result);
+		response->setBody(result);
 	}
 	close(file_fd);
+	free(result_buf);
 }
 
 void CGI::setPathCGI(char * string) {
